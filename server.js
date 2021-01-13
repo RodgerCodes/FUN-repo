6e48d7c50ecda9bfd65ce1037733899ea22495ce
Rodger Kumwanje <rkumwanje@gmail.com>
2021-01-13 13:58:20 +0000

    done with password reset

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require("path");
 const passport = require("passport");
 const session = require("express-session");
 const flash = require("express-flash");
+const methodOverride = require("method-override");
 const SequeliStore = require("connect-session-sequelize")(session.Store);
 const app = express();
 const init = require("./config/passport");
@@ -24,6 +25,18 @@ app.use(express.json());
 app.use(
   express.urlencoded({
     extended: "false",
+  })
+);
+
+// method override middleware
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
   })
 );
 
@@ -53,6 +66,10 @@ app.engine(
   exphs({
     defaultLayout: "main",
     extname: ".hbs",
+    runtimeOptions: {
+      allowProtoPropertiesByDefault: true,
+      allowProtoMethodsByDefault: true,
+    },
   })
 );
 app.set("view engine", ".hbs");
